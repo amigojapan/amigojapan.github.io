b0af6675c577208022a5053a69ef8817d1ef98b7
Usmar A Padow <usmpadow@gmail.com>
2022-09-14 10:14:04 +0900

    Update more_or_less.py

--- 8_basics_of_programming/more_or_less.py
+++ 8_basics_of_programming/more_or_less.py
@@ -1,9 +1,9 @@
 x=5
 if x>3:
-  print("x is more than 3")
-  print("I wish x was less than 3")
+    print("x is more than 3")
+    print("I wish x was less than 3")
 if x<3:
-  print("this line wont get printed")
+    print("this line wont get printed")
 x=2
 if x<3:
-  print("x is less than 3")
+    print("x is less than 3")
